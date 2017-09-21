3b141171fb5d2e2618fb67fe2555a7656d68f081
Hack Reactor Student <student@hackreactor.com>
2017-09-20 17:32:24 -0700

    Implement Cookie Parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,21 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  console.log(req.headers);
+  //console.log('CREATE SESSION');
+  // Promise.resolve(req.cookies.shortlyid)
+  //   .then((hash) => {
+  //     if (hash === undefined) {
+  //       throw hash;
+  //     }
+  //     console.log(cookie);
+  //   })
+  //   .catch(() => {
+  //     console.log('********* Creating New Session ************');
+  //     console.log(models.Sessions.create());
+  //   });
+
+  console.log(models.Sessions.get());
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,22 @@
+const _ = require('lodash');
+
+const parseData = data => {
+  return _.reduce(data, (acc, value, index) => {
+    var tuple = value.split('=');
+    acc[tuple[0].trim()] = tuple[1].trim();
+    return acc;
+  }, {});
+};
+
 const parseCookies = (req, res, next) => {
+  //console.log(req.headers.cookie);
+  if (req.headers.cookie) {
+    var cookies = req.headers.cookie.split(';');    
+    req.cookies = parseData(cookies);
+    //console.log(parseData(cookies));
+  }
+  next();
 };
 
 module.exports = parseCookies;
+
