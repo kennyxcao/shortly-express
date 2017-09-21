23f0b83be6ab30010dde57c7852a9c53c43d04e8
Hack Reactor Student <student@hackreactor.com>
2017-09-20 19:10:01 -0700

    Implement and pass all specs for Session Parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,21 +2,26 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  console.log(req.headers);
-  //console.log('CREATE SESSION');
-  // Promise.resolve(req.cookies.shortlyid)
-  //   .then((hash) => {
-  //     if (hash === undefined) {
-  //       throw hash;
-  //     }
-  //     console.log(cookie);
-  //   })
-  //   .catch(() => {
-  //     console.log('********* Creating New Session ************');
-  //     console.log(models.Sessions.create());
-  //   });
-
-  console.log(models.Sessions.get());
+  Promise.resolve(req.cookies.shortlyid)
+    .then((hash) => {
+      if (hash === undefined) {
+        return models.Sessions.create()
+                .then((newEntry) => {
+                  return models.Sessions.get({id: newEntry.insertId});
+                });
+      } 
+      return models.Sessions.get({hash: hash});
+    })
+    .then((session) => {
+      req.session = session;
+      res.cookie('shortlyid', session.hash);
+      next();
+    })
+    .catch((error) => {
+      console.log('++++++++++++++ Create Session Error +++++++++++++++++++');
+      req.cookies = {};
+      module.exports.createSession(req, res, next);
+    });
 };
 
 /************************************************************/
